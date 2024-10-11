704bba96a6c96a95586470f6fce04a7957b0d7a7
Kigo-P <peterkigo33@gmail.com>
2024-10-11 08:40:10 +0300

    completed lab

--- server/app.py
+++ server/app.py
@@ -47,6 +47,64 @@ class ShowArticle(Resource):
             return make_response(article_json, 200)
 
         return {'message': 'Maximum pageview limit reached'}, 401
+    
+#  creating a Login resource
+class Login(Resource):
+    #  a post method  to get the username and set the user to a session 
+    def post(self):
+        # querying and filtering by the user by the username since there is no password yet
+        user = User.query.filter(User.username == request.get_json()["username"]).first()
+
+        # setting the session using the id
+        session["user_id"] = user.id
+        #  creating and returning a  response
+        response_dict = user.to_dict()
+        response = make_response(response_dict,200)
+        return response
+    pass
+
+# add the Login resource to the api
+api.add_resource(Login, "/login")
+
+# creating a Logout resource
+class Logout(Resource):
+    # a delete method to clear out the user once they are logged out
+    def delete(self):
+        # setting the session hash to none 
+        session["user_id"] = None
+        #  creating and returning a  response
+        response_body = {"msg": "No data: 204(No Content)"}
+        response = make_response(response_body,204)
+        return response
+    pass
+
+# adding the Logout resource to the api
+api.add_resource(Logout, "/logout")
+
+# creating a CheckSession resource 
+class CheckSession(Resource):
+    #  a get method that retrieves the user_id from the session
+    def get(self):
+        
+        #  using query and filter to get the session using the users id
+        user = User.query.filter(User.id == session.get("user_id")).first()
+
+        #  if the session has a user id return the user else return an error
+        if user:
+            #  creating and returning a  response
+            response_dict = user.to_dict()
+            response = make_response(response_dict,200)
+            return response
+        else:
+            #  creating and returning a  response
+            response_body = {}
+            response = make_response(response_body,401)
+            return response
+
+    pass
+
+# add the CheckSession resource to the api
+api.add_resource(CheckSession, "/check_session")
 
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
